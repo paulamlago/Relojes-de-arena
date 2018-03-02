ed859868e7de63e5e62b753cf421badfd656ebb1
paulamlago <pmunoz06@ucm.es>
2018-03-02 10:30:29 +0100

    Empty clock issue fixed

--- practica7-relojes.pl
+++ practica7-relojes.pl
@@ -10,15 +10,6 @@ objetivo(estado(reloj1, 4, reloj2, _)). %tienen 3 minutos en el lado inferior
 objetivo(estado(reloj1, _, reloj2, 8)).
 
 %operaciones
-%al girar un reloj u otro el tiempo restante en el contrario se mantiene
-
-movimiento(estado(reloj1, Y, reloj2, X), 
-	  estado(reloj1, Z, reloj2, X),
-	  girarReloj(reloj1)):- Z is 7 - Y, \+vacio(Z).
-
-movimiento(estado(reloj1, X, reloj2, Y), 
-	  estado(reloj1, X, reloj2, Z),
-	  girarReloj(reloj2)):- Z is 11 - Y, \+vacio(Z).
 
 %puesto que el tiempo pasa igual para ambos relojes, no necesitaremos más que 
 %una función que les reste el tiempo a los dos a la vez, siempre comprobando que 
@@ -26,13 +17,24 @@ movimiento(estado(reloj1, X, reloj2, Y),
 
 movimiento(estado(reloj1, X, reloj2, Y),
 		estado(reloj1, Z, reloj2, W),
-		restarTiempo) :- Z is X - 1, W is Y - 1, \+vacio(Z), \+vacio(W).
+		restarTiempo) :- \+vacio(X), \+vacio(Y), Z is X - 1, W is Y - 1.
+
+%al girar un reloj u otro el tiempo restante en el contrario se mantiene
+
+movimiento(estado(reloj1, Y, reloj2, X), 
+	  estado(reloj1, Z, reloj2, X),
+	  girarReloj(reloj1)):- Z is 7 - Y.
+
+movimiento(estado(reloj1, X, reloj2, Y), 
+	  estado(reloj1, X, reloj2, Z),
+	  girarReloj(reloj2)):- Z is 11 - Y.
+
 
 %esta regla comprueba que el valor de X no sea 0, o menor, o sea, que el reloj no 
 %se haya quedado sin arena. Si es así aplicamos el corte para detener la búsqueda de 
 %más soluciones por esta rama.
 
-vacio(X) :- (X =< 0), !.
+vacio(X) :- (X =< 0).
 
 % Encontrar la secuencia de movimientos
 puede(Estado,_, [], [Estado]) :- objetivo(Estado), nl, write('Hemos llegado al OBJETIVO: '), write(Estado).
