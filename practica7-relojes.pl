97f6103924b35c8c851b4edaad585f00c8bab6ea
paulamlago <pmunoz06@ucm.es>
2018-03-04 12:09:45 +0100

    Dejamos caer arena de golpe

--- practica7-relojes.pl
+++ practica7-relojes.pl
@@ -1,25 +1,24 @@
 %estado_inicial
 %representacion: tiempo que le queda en la parte superior al primer reloj, tiempo que le queda en la parte superior al reloj2
 
-inicial(estado(7.0, 11.0)).
+inicial(estado(7, 11)).
 
 %estados_finales
 
-objetivo(estado(3.0, _)). %tienen 3 minutos en el lado superior
-objetivo(estado(_, 3.0)).
-objetivo(estado(4.0, _)). %tienen 3 minutos en el lado inferior
-objetivo(estado(_, 8.0)).
+objetivo(estado(3, _)). %tienen 3 minutos en el lado superior
+objetivo(estado(_, 3)).
+objetivo(estado(4, _)). %tienen 3 minutos en el lado inferior
+objetivo(estado(_, 8)).
 
 %operaciones
 
-%puesto que el tiempo pasa igual para ambos relojes, no necesitaremos más que 
-%una función que les reste el tiempo a los dos a la vez, siempre comprobando que 
-%tengan arena con la funcion vacio. Si el reloj no tiene arena la unica operacion 
-%posible es girar el reloj.
+%Si el reloj no tiene arena la unica operacion posible es girar el reloj.
+%Restamos el reloj que menos arena tiene al que mas tiene y el menor lo dejamos a 0.
+%Dejamos caer arena de golpe, hasta que uno llega a 0, punto en el que se bloquea la accion.
 
 movimiento(estado(X, Y),
 		estado(Z, W),
-		restarTiempo) :- Z is X - (1 / 60), W is Y - (1 / 60), \+vacio(Z), \+vacio(W).
+		caeArena) :- \+ vacio(X), \+ vacio(Y), C is min(X, Y), Z is X - C, W is Y - C.
 
 
 %al girar un reloj u otro, el tiempo restante en el contrario se mantiene
@@ -27,11 +26,11 @@ movimiento(estado(X, Y),
 
 movimiento(estado(Y, X),
 	  estado(Z, X),
-	  girarReloj1):- Z is 7.0 - Y.
+	  girarMenor):- Z is 7 - Y.
 
 movimiento(estado(X, Y), 
 	  estado(X, Z),
-	  girarReloj2):- Z is 11.0 - Y.
+	  girarReloj2):- Z is 11 - Y.
 
 %esta regla comprueba que el valor de X no sea 0, o menor para mayor seguridad, o sea, que el reloj no 
 %se haya quedado sin arena.
